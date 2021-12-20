2fcacfe2d8ea4908e69b12481429fd976239fbda
Karishma Shah <shah.karishma02@gmail.com>
2021-12-20 18:38:59 -0500

    transfer tokens

--- contracts/KCoin.sol
+++ contracts/KCoin.sol
@@ -7,6 +7,8 @@ contract KCoin {
     string public standard = "K-Coin v1.0";
     uint256 public totalSupply;
 
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
     //map(key => value): who has each token
     mapping(address => uint256) public balanceOf;
 
@@ -14,5 +16,21 @@ contract KCoin {
         //msg.sender is the account that deployed the contract
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
+    }
+
+    // Transfer
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
