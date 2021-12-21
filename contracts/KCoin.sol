a17111d56a523a9affeb827499a7656e7291f8c4
Karishma Shah <shah.karishma02@gmail.com>
2021-12-20 20:38:30 -0500

    delegated trasfer

--- contracts/KCoin.sol
+++ contracts/KCoin.sol
@@ -8,17 +8,21 @@ contract KCoin {
     uint256 public totalSupply;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
     //map(key => value): who has each token
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initialSupply) {
         //msg.sender is the account that deployed the contract
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
-
-    // Transfer
 
     function transfer(address _to, uint256 _value)
         public
@@ -30,6 +34,35 @@ contract KCoin {
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
 
         return true;
     }
